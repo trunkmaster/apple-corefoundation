d8f79696d8ed47044bc9b2236b61f3d0edcdf6d7
Sergii Stoian <stoyan255@gmail.com>
2024-02-01 02:20:29 +0200

    added support files

---
+++ CoreFoundation/cmake.sh
@@ -0,0 +1,11 @@
+cmake .. \
+	-DCMAKE_C_COMPILER=clang \
+        -DCMAKE_SHARED_LINKER_FLAGS="-L/usr/NextSpace/lib -luuid" \
+        -DCF_DEPLOYMENT_SWIFT=NO \
+        -DBUILD_SHARED_LIBS=YES \
+        -DCMAKE_INSTALL_PREFIX=/usr/NextSpace \
+        -DCMAKE_INSTALL_LIBDIR=/usr/NextSpace/lib \
+        -DCMAKE_LIBRARY_PATH=/usr/NextSpace/lib \
+        \
+        -DCMAKE_SKIP_RPATH=ON \
+        -DCMAKE_BUILD_TYPE=Debug
